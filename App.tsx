c1dbc2d219bdbef151eb91f1c72d739a2b42f9bf
29Marina <ad.marina@hotmail.fr>
2022-06-27 14:44:59 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+
+export default function App() {
+
+  return (
+    
+    <div> Router </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-qar5ok
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-qar5ok)
